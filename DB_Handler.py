d38a750fcabf19ff9664b520499a70f30d6edb27
ofir <ofirgr12@gmail.com>
2024-01-08 16:37:41 +0200

    ofir

--- DB_Handler.py
+++ DB_Handler.py
@@ -1,19 +1,90 @@
 import sqlite3
 
-conn = sqlite3.connect('users.db')
+def create_table():
+    conn = sqlite3.connect('users.db')
 
-cursor = conn.cursor()
+    cursor = conn.cursor()
 
-create_table_query = '''
-CREATE TABLE IF NOT EXISTS users (
-    username TEXT PRIMARY KEY,
-    password TEXT NOT NULL
-);
-'''
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT NOT NULL
+    );
+    
+    """)
 
-cursor.execute(create_table_query)
+    conn.commit()
+    conn.close()
 
-conn.commit()
-conn.close()
+def check_password(username , given_password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        '''
+        SELECT password FROM users
+        WHERE username=?
+        ''',(username,)
+    )
+    correct_password = cursor.fetchone()[0]
+
+    conn.commit()
+    conn.close()
+
+    return correct_password == given_password
+def add_user(username, password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+            INSERT INTO users (username, password)
+            VALUES (?, ?);
+        """,(username, password))
+
+    conn.commit()
+    conn.close()
+
+def remove_user(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                DELETE FROM users 
+                WHERE username = ?;
+                """,(username,));
+
+    conn.commit()
+    conn.close()
+
+def change_password(username , password):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                UPDATE users
+                SET password = ?
+                WHERE username = ?;
+            """, (password,username))
+
+    conn.commit()
+    conn.close()
+
+
+
+
+
+if __name__=="__main__":
+    create_table()
+    #add_user('admin','1234')
+    #print(check_password('admin' ,'12345'))
+    #remove_user('admin')
+    #change_password('admin1','admin')
+
+
+
+
+
+
+
 
 
